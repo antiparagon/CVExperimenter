d63055a17838a2f8565f6614a2c8c23679e39a1e
Bill McKay <antiparagon@gmail.com>
2016-05-20 06:30:45 -0400

    Updated ImageEditorTab

--- src/main/scala/com/antiparagon/cvexperimenter/ImageEditorTab.scala
+++ src/main/scala/com/antiparagon/cvexperimenter/ImageEditorTab.scala
@@ -6,16 +6,23 @@ import scalafx.scene.control.{Button, Label, ScrollPane, Tab}
 import scalafx.scene.image.{Image, ImageView}
 import scalafx.scene.input.MouseEvent
 import scalafx.scene.layout.{HBox, VBox}
+import scalafx.scene.text.{Font, FontWeight}
 
 /**
   * Created by wmckay on 5/14/16.
   */
 class ImageEditorTab(val img : Image) extends Tab with ExperimenterTab {
 
-  val mousePosLabel = new Label("x: 0 y: 0")
+  val mousePosLabel = new Label {
+    text = "x: 0 y: 0"
+    font = Font.font(null, FontWeight.Bold, 18)
+    padding = Insets(50)
+  }
+
   val currentFrame =  new ImageView(img)
   currentFrame.setOnMouseMoved((e : MouseEvent) => { mousePosLabel.text = s"[${e.getX()}, ${e.getY()}]"; })
   currentFrame.setOnMouseClicked((e : MouseEvent) => { println(s"[${e.getX()}, ${e.getY()}]"); })
+
   val editButton = new Button {
     text = "Edit"
     style = BUTTON_STYLE
