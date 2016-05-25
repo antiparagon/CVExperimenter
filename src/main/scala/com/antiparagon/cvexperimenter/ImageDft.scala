651ca15beb8c7f992b8077a7e9c3679df0423bc0
Bill McKay <antiparagon@gmail.com>
2016-05-25 06:43:21 -0400

    Worked on ImageDft

--- src/main/scala/com/antiparagon/cvexperimenter/ImageDft.scala
+++ src/main/scala/com/antiparagon/cvexperimenter/ImageDft.scala
@@ -1,6 +1,9 @@
 package com.antiparagon.cvexperimenter
 
-import org.opencv.core.{Core, Mat, Scalar}
+import java.util
+
+import collection.JavaConverters._
+import org.opencv.core._
 
 
 /**
@@ -8,17 +11,71 @@ import org.opencv.core.{Core, Mat, Scalar}
   */
 object ImageDft {
 
+  def antitransformImage(complexImage: Mat, planes: List[Mat]): Mat = {
+    Core.idft(complexImage, complexImage)
+
+    val restoredImage = new Mat()
+    Core.split(complexImage, planes.asJava)
+    Core.normalize(planes(0), restoredImage, 0, 255, Core.NORM_MINMAX)
+    return restoredImage
+  }
+
   def optimizeImageDim(image: Mat): Mat =  {
     // init
     val padded = new Mat()
     // get the optimal rows size for dft
-    val addPixelRows = Core.getOptimalDFTSize(image.rows());
+    val addPixelRows = Core.getOptimalDFTSize(image.rows)
     // get the optimal cols size for dft
-    val addPixelCols = Core.getOptimalDFTSize(image.cols());
+    val addPixelCols = Core.getOptimalDFTSize(image.cols)
     // apply the optimal cols and rows size to the image
-    Core.copyMakeBorder(image, padded, 0, addPixelRows - image.rows(), 0, addPixelCols - image.cols(), Core.BORDER_CONSTANT, Scalar.all(0));
+    Core.copyMakeBorder(image, padded, 0, addPixelRows - image.rows, 0, addPixelCols - image.cols, Core.BORDER_CONSTANT, Scalar.all(0))
 
-    return padded;
+    return padded
   }
 
+  def createOptimizedMagnitude(complexImage: Mat): Mat = {
+    // init
+    val newPlanes: java.util.List[Mat] = new java.util.ArrayList[Mat]
+    val mag = new Mat()
+    // split the comples image in two planes
+    Core.split(complexImage, newPlanes)
+    // compute the magnitude
+    Core.magnitude(newPlanes.get(0), newPlanes.get(1), mag)
+
+    // move to a logarithmic scale
+    Core.add(Mat.ones(mag.size(), CvType.CV_32F), mag, mag)
+    Core.log(mag, mag)
+    // optionally reorder the 4 quadrants of the magnitude image
+    val newMag = shiftDFT(mag)
+    // normalize the magnitude image for the visualization since both JavaFX
+    // and OpenCV need images with value between 0 and 255
+    // convert back to CV_8UC1
+    newMag.convertTo(newMag, CvType.CV_8UC1)
+    Core.normalize(newMag, newMag, 0, 255, Core.NORM_MINMAX, CvType.CV_8UC1)
+
+    // you can also write on disk the resulting image...
+    // Imgcodecs.imwrite("../magnitude.png", mag);
+
+    return newMag
+  }
+
+  def shiftDFT(inImage: Mat): Mat =  {
+    val image = inImage.submat(new Rect(0, 0, inImage.cols & -2, inImage.rows & -2))
+    val cx = inImage.cols / 2
+    val cy = inImage.rows / 2
+
+    val q0 = new Mat(image, new Rect(0, 0, cx, cy))
+    val q1 = new Mat(image, new Rect(cx, 0, cx, cy))
+    val q2 = new Mat(image, new Rect(0, cy, cx, cy))
+    val q3 = new Mat(image, new Rect(cx, cy, cx, cy))
+
+    val tmp = new Mat()
+    q0.copyTo(tmp)
+    q3.copyTo(q0)
+    tmp.copyTo(q3)
+
+    q1.copyTo(tmp)
+    q2.copyTo(q1)
+    tmp.copyTo(q2)
+  }
 }
