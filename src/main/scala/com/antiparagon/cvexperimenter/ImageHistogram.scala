5873a0c0b9789ad9ce25e0e4ab49877d0d9b4fce
Bill McKay <antiparagon@gmail.com>
2016-06-02 06:45:47 -0400

    Debugging ImageHistogram

--- src/main/scala/com/antiparagon/cvexperimenter/ImageHistogram.scala
+++ src/main/scala/com/antiparagon/cvexperimenter/ImageHistogram.scala
@@ -43,8 +43,9 @@ object ImageHistogram {
 
     println("histSize.get(0, 0)(0): " + histSize.get(0, 0)(0))
     println("bin_w: " + bin_w)
-
+    
     val histImage = new Mat(hist_h, hist_w, CvType.CV_8UC3, new Scalar(0, 0, 0))
+    println(s"histImage.rows(): ${histImage.rows()}")
     // normalize the result to [0, histImage.rows()]
     Core.normalize(hist_b, hist_b, 0, histImage.rows(), Core.NORM_MINMAX, -1, new Mat())
 
@@ -59,7 +60,8 @@ object ImageHistogram {
     for (i <- 1 until (histSize.get(0, 0)(0)).toInt) {
 
       println(s"bin_w * (i - 1): ${bin_w * (i - 1)}")
-      println(s"Math.round(hist_b.get(i - 1, 0)(0)): ${hist_h - Math.round(hist_b.get(i - 1, 0)(0))}")
+      println(s"hist_b.get(i - 1, 0)(0): ${hist_b.get(i - 1, 0)(0)}")
+      println(s"Math.round(hist_h - hist_b.get(i - 1, 0)(0)): ${hist_h - Math.round(hist_b.get(i - 1, 0)(0))}")
       println()
       // B component or gray image
       Imgproc.line(histImage, new Point(bin_w * (i - 1), hist_h - Math.round(hist_b.get(i - 1, 0)(0))),
