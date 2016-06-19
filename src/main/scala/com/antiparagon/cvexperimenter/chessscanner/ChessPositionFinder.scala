b6d2318c5ba417721dcff9528eaeabb2b445aef1
Bill McKay <antiparagon@gmail.com>
2016-06-18 22:11:15 -0400

    Mapped out the algorithm for getting the chess position

--- src/main/scala/com/antiparagon/cvexperimenter/chessscanner/ChessPositionFinder.scala
+++ src/main/scala/com/antiparagon/cvexperimenter/chessscanner/ChessPositionFinder.scala
@@ -9,12 +9,25 @@ object ChessPositionFinder {
 
   /**
     * Takes an image of a chessboard and returns the position in FEN notation. This
-    * function assumes that the full image is the chessboard.
+    * function assumes that the full image is just the chessboard.
     *
     * @param inImg of chessboard
     * @return string FEN position
     */
   def getFenPosition(inImg: Mat): Option[String] = {
+
+    // Get square coordinates, find all 64 squares
+
+
+    // Find all the pieces
+
+
+    // Use the squares coordinates to determine the square the pieces are on
+
+
+    // Use the position to create the FEN string
+
+
     Option("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
   }
 
