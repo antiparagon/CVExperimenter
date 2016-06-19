cf44e07064a771a329d5538d76480e959d82c37a
Bill McKay <antiparagon@gmail.com>
2016-06-19 10:08:12 -0400

    Determined logic to find chess piece position

--- src/main/scala/com/antiparagon/cvexperimenter/chessscanner/ChessPieceFinder.scala
+++ src/main/scala/com/antiparagon/cvexperimenter/chessscanner/ChessPieceFinder.scala
@@ -5,4 +5,30 @@ package com.antiparagon.cvexperimenter.chessscanner
   */
 object ChessPieceFinder {
 
+  // Function to take an image and a matrix of chess square coordinates
+
+  // Find all pawn coordinates
+
+  // Determine color
+
+  // Find rook coordinates
+
+  // Determine color
+
+  // Find knight coordinates
+
+  // Determine color
+
+  // Find bishop coordinates
+
+  // Determine color
+
+  // Find queen coordinates
+
+  // Determine color
+
+  // Find king coordinates
+
+  // Determine color
+
 }
