d5e38f9fdc16071f2a3c25f4c62130a8c77ff80c
Bill McKay <antiparagon@gmail.com>
2016-07-08 06:23:14 -0400

    Added steps to ChessScannerTab

--- src/main/scala/com/antiparagon/cvexperimenter/gui/ChessScannerTab.scala
+++ src/main/scala/com/antiparagon/cvexperimenter/gui/ChessScannerTab.scala
@@ -13,14 +13,37 @@ import scalafx.scene.layout.{HBox, VBox}
   */
 class ChessScannerTab(val img : Image) extends Tab with ExperimenterTab {
 
+  val STEP1_TEXT = "Find Chessboard"
+  val STEP2_TEXT = "Find Squares"
+  val STEP3_TEXT = "Find Pieces"
+  val STEP4_TEXT = "Get Position"
 
   val imgView =  new ImageView(img)
 
   val startButton = new Button {
-    text = "Start"
+    text = STEP1_TEXT
     style = BUTTON_STYLE
     onAction = handle {
-      println("Pushed button")
+      text.value match {
+        case STEP1_TEXT => {
+          println(STEP1_TEXT)
+          text = STEP2_TEXT
+        }
+        case STEP2_TEXT => {
+          println(STEP2_TEXT)
+          text = STEP3_TEXT
+        }
+        case STEP3_TEXT => {
+          println(STEP3_TEXT)
+          text = STEP4_TEXT
+        }
+        case STEP4_TEXT => {
+          println(STEP4_TEXT)
+          text = "Done"
+        }
+        case _ => {}
+      }
+
     }
   }
 
