3916f80f4d730c4cb4a8ff62cc66411c6f3be282
Bill McKay <antiparagon@gmail.com>
2016-08-02 06:30:30 -0400

    Using Otsu's binarization for thresholding

--- src/main/scala/com/antiparagon/cvexperimenter/chessscanner/ChessBoardFinder.scala
+++ src/main/scala/com/antiparagon/cvexperimenter/chessscanner/ChessBoardFinder.scala
@@ -2,6 +2,7 @@ package com.antiparagon.cvexperimenter.chessscanner
 
 import java.util
 
+import com.antiparagon.cvexperimenter.CVExperimenter
 import com.antiparagon.cvexperimenter.tools.ImageTools
 import org.opencv.calib3d.Calib3d
 import org.opencv.core.{CvType, MatOfPoint2f, _}
@@ -54,8 +55,10 @@ object ChessboardFinder {
 
     val tempImg = new Mat
     Imgproc.cvtColor(inImg, tempImg, Imgproc.COLOR_BGR2GRAY)
-    Imgproc.adaptiveThreshold(tempImg, tempImg, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 11, 2)
+    //Imgproc.adaptiveThreshold(tempImg, tempImg, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 11, 2)
+    Imgproc.threshold(tempImg, tempImg, 0, 255, Imgproc.THRESH_BINARY + Imgproc.THRESH_OTSU)
     //return Some(tempImg)
+    CVExperimenter.tabManager.addImageTab("Threshold image", ImageTools.convertCVtoFX(tempImg))
 
 
     val boardSize = new Size(7, 7)
