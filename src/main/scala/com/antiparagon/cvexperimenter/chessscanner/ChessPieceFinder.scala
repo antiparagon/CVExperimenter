408ec4b5b3987ae6ef36b7b764c538b6319b3bb3
Bill McKay <antiparagon@gmail.com>
2016-08-24 06:26:30 -0400

    Added feature detector to ChessPieceFinder

--- src/main/scala/com/antiparagon/cvexperimenter/chessscanner/ChessPieceFinder.scala
+++ src/main/scala/com/antiparagon/cvexperimenter/chessscanner/ChessPieceFinder.scala
@@ -1,6 +1,7 @@
 package com.antiparagon.cvexperimenter.chessscanner
 
-import org.opencv.core.{Mat, Rect}
+import org.opencv.core.{Mat, MatOfKeyPoint, Rect}
+import org.opencv.features2d.FeatureDetector
 import org.opencv.imgcodecs.Imgcodecs
 import org.opencv.imgproc.Imgproc
 
@@ -37,12 +38,20 @@ object ChessPieceFinder {
   def findChessPieces(chessboard: Chessboard, boardImg: Mat): Int = {
     var piecesFound = 0
 
+    val features = FeatureDetector.create(FeatureDetector.FAST)
     chessboard.getSquares().foreach(square => {
       val squareImg = new Mat(boardImg, square.rect)
+
+      val keyPoints = new MatOfKeyPoint()
+      features.detect(squareImg, keyPoints)
+
       val (col, row) = chessboard.translateMatrixCoor(square.row, square.column)
       val coorStr = col + row.toString
+
+      println(s"$coorStr: ${keyPoints.size()}")
+
+
       val imgPath = "ChessSquares/" + coorStr + ".png"
-      println(imgPath)
       Imgcodecs.imwrite(imgPath, squareImg)
     })
 
