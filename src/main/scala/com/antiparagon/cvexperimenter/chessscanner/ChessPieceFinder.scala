59c67df19c4a1a6aec285b6db11139d9a859c2c5
Bill McKay <antiparagon@gmail.com>
2016-09-08 07:33:16 -0400

    Hard coded key point counts for starting position

--- src/main/scala/com/antiparagon/cvexperimenter/chessscanner/ChessPieceFinder.scala
+++ src/main/scala/com/antiparagon/cvexperimenter/chessscanner/ChessPieceFinder.scala
@@ -99,25 +99,52 @@ object ChessPieceFinder {
     if(keyPoints.size().height > 5) {
       val points = keyPoints.size().height
       var piece = "P"
-      if(points == 19) {
-        piece = "p"
-      } else if(points == 51) {
-        piece = "k"
-      } else if(points == 35) {
+      if(points == 27) {
+        piece = "r"
+      } else if(points == 24) {
+        piece = "n"
+      } else if(points == 36) {
         piece = "b"
+      } else if(points == 31) {
+        piece = "q"
+      } else if(points == 57) {
+        piece = "k"
+      } else if(points == 32) {
+        piece = "b"
+      } else if(points == 22) {
+        piece = "n"
+      } else if(points == 17) {
+        piece = "r"
+      } else if(points == 20) {
+        piece = "p"
+      } else if(points == 19) {
+        piece = "p"
+      }
+
+      else if(points == 15) {
+        piece = "R"
+      } else if(points == 38) {
+        piece = "N"
+      } else if(points == 24) {
+        piece = "B"
+      } else if(points == 59) {
+        piece = "Q"
+      } else if(points == 42) {
+        piece = "K"
+      } else if(points == 40) {
+        piece = "B"
       } else if(points == 34) {
         piece = "N"
-      } else if(points == 26) {
-        piece = "P"
+      } else if(points == 22) {
+        piece = "R"
       } else if(points == 32) {
         piece = "P"
-      } else if(points == 27) {
-        piece = "r"
-      } else if(points == 52) {
-        piece = "K"
-      } else if(points == 17) {
-        piece = "R"
+      } else if(points == 24) {
+        piece = "P"
+      } else if(points == 26) {
+        piece = "P"
       }
+
       Some(piece)
     } else {
       None
