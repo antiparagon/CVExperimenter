8bbfc65e04e026bc90049432c430edd68d6f97db
Bill McKay <antiparagon@gmail.com>
2016-11-20 14:04:40 -0500

    Added passing test for ChessboardFinderContoursAlgorithm.scanRectList()

--- src/test/scala/com/antiparagon/cvexperimenter/chessscanner/ChessboardFinderContoursAlgorithmTest.scala
+++ src/test/scala/com/antiparagon/cvexperimenter/chessscanner/ChessboardFinderContoursAlgorithmTest.scala
@@ -63,10 +63,21 @@ class ChessboardFinderContoursAlgorithmTest extends FlatSpec with Matchers {
   "ChessboardFinderContoursAlgorithm.scanRectList()" should "return None given Rect list less then 4 Rect" in {
     val rectList = ArrayBuffer[Rect]()
     rectList += new Rect(0, 0, 100, 100)
-    rectList += new Rect(10, 20, 100, 100)
-    rectList += new Rect(10, 20, 100, 100)
+    rectList += new Rect(0, 0, 10, 10)
+    rectList += new Rect(10, 0, 10, 10)
     val rect = ChessboardFinderContoursAlgorithm().scanRectList(rectList)
     rect should be (None)
+  }
+
+  "ChessboardFinderContoursAlgorithm.scanRectList()" should "return Rect(0, 0, 100, 100) given Rect list with three inner Rects" in {
+    val rectList = ArrayBuffer[Rect]()
+    rectList += new Rect(0, 0, 64, 64)
+    rectList += new Rect(0, 0, 8, 8)
+    rectList += new Rect(0, 8, 8, 8)
+    rectList += new Rect(0, 16, 8, 8)
+    rectList += new Rect(0, 24, 8, 8)
+    val rect = ChessboardFinderContoursAlgorithm().scanRectList(rectList)
+    rect.get should be (new Rect(0, 0, 64, 64))
   }
 
 }
