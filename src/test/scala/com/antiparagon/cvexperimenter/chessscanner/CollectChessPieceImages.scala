54759c9171bb60e4a2697d93f515ca8ecb5eaca6
Bill McKay <antiparagon@gmail.com>
2017-01-07 15:05:28 -0500

    Working on CollectChessPieceImages helper script

--- src/test/scala/com/antiparagon/cvexperimenter/chessscanner/CollectChessPieceImages.scala
+++ src/test/scala/com/antiparagon/cvexperimenter/chessscanner/CollectChessPieceImages.scala
@@ -1,13 +1,31 @@
 package com.antiparagon.cvexperimenter.chessscanner
 
+import java.nio.file.{Files, Paths}
+
 
 /**
   * Created by wmckay on 1/6/17.
   */
 object CollectChessPieceImages {
 
+  val IMG_FOLDER = "ChessSquares/"
+
   def main(args: Array[String]): Unit = {
-    //val rooksFolder = Path("Rook")
+    createFolders()
   }
 
+  def createFolders(): Unit = {
+    val rookFolder = Paths.get(IMG_FOLDER + "Rook")
+    Files.createDirectories(rookFolder)
+    val knightFolder = Paths.get(IMG_FOLDER + "Knight")
+    Files.createDirectories(knightFolder)
+    val bishopFolder = Paths.get(IMG_FOLDER + "Bishop")
+    Files.createDirectories(bishopFolder)
+    val queenFolder = Paths.get(IMG_FOLDER + "Queen")
+    Files.createDirectories(queenFolder)
+    val kingFolder = Paths.get(IMG_FOLDER + "King")
+    Files.createDirectories(kingFolder)
+    val pawnFolder = Paths.get(IMG_FOLDER + "Pawn")
+    Files.createDirectories(pawnFolder)
+  }
 }
