9b99466e08b232c5e2facb7329617963eee3d16a
Bill McKay <antiparagon@gmail.com>
2017-01-14 14:40:53 -0500

    Working on ChessPieceClassifierFast

--- src/main/scala/com/antiparagon/cvexperimenter/chessscanner/ChessPieceClassifierFast.scala
+++ src/main/scala/com/antiparagon/cvexperimenter/chessscanner/ChessPieceClassifierFast.scala
@@ -1,8 +1,8 @@
 package com.antiparagon.cvexperimenter.chessscanner
 
 import com.antiparagon.cvexperimenter.tools.ImageTools
-import org.opencv.core.{Mat, MatOfDMatch, MatOfKeyPoint, Scalar}
-import org.opencv.features2d.{FeatureDetector, Features2d}
+import org.opencv.core.{Mat, MatOfKeyPoint}
+import org.opencv.features2d.{FeatureDetector}
 import org.opencv.imgcodecs.Imgcodecs
 
 
@@ -49,8 +49,6 @@ class ChessPieceClassifierFast {
   def classifyPiece(inputImg: Mat, coorStr: String): Option[String] = {
 
     val squareImg = ImageTools.resize(inputImg, 50, 50)
-    //Imgproc.cvtColor(squareImg, squareImg, Imgproc.COLOR_BGR2GRAY)
-    //Imgproc.threshold(squareImg, squareImg, 0, 255, Imgproc.THRESH_BINARY + Imgproc.THRESH_OTSU)
 
     val keyPointsMat = new MatOfKeyPoint()
     features.detect(squareImg, keyPointsMat)
@@ -62,9 +60,7 @@ class ChessPieceClassifierFast {
     var x = 0.0
     var y = 0.0
     var resp = 0.0
-    val NL = System.lineSeparator()
     keyPoints.foreach(kp => {
-      println(s"${kp}")
       x += kp.pt.x
       y += kp.pt.y
       resp += kp.response.toDouble
@@ -92,63 +88,5 @@ class ChessPieceClassifierFast {
     } else {
       None
     }
-  }
-
-  def run() {
-    import scala.reflect._
-    import org.opencv.features2d.{DescriptorExtractor, DescriptorMatcher, FeatureDetector, Features2d}
-    import org.opencv.imgcodecs.Imgcodecs
-    import org.opencv.imgproc.Imgproc
-
-    println(s"\nRunning ${classTag[this.type].toString.replace("$", "")}")
-
-    // Detects keypoints and extracts descriptors in a given image of type Mat.
-    def detectAndExtract(mat: Mat) = {
-      // A special container class for KeyPoint.
-      val keyPoints = new MatOfKeyPoint
-      // We're using the ORB detector.
-      val detector = FeatureDetector.create(FeatureDetector.ORB)
-      detector.detect(mat, keyPoints)
-
-      println(s"There were ${keyPoints.toArray.size} KeyPoints detected")
-
-      // Let's just use the best KeyPoints.
-      val sorted = keyPoints.toArray.sortBy(_.response).reverse.take(50)
-      // There isn't a constructor that takes Array[KeyPoint], so we unpack
-      // the array and use the constructor that can take any number of
-      // arguments.
-      val bestKeyPoints: MatOfKeyPoint = new MatOfKeyPoint(sorted: _*)
-
-      // We're using the ORB descriptor.
-      val extractor = DescriptorExtractor.create(DescriptorExtractor.ORB)
-      val descriptors = new Mat
-      extractor.compute(mat, bestKeyPoints, descriptors)
-
-      println(s"${descriptors.rows} descriptors were extracted, each with dimension ${descriptors.cols}")
-
-      (bestKeyPoints, descriptors)
-    }
-
-    // Load the images from the |resources| directory.
-    val leftImage = Imgcodecs.imread("img1.png")
-    val rightImage = Imgcodecs.imread("img2.png")
-
-    // Detect KeyPoints and extract descriptors.
-    val (leftKeyPoints, leftDescriptors) = detectAndExtract(leftImage)
-    val (rightKeyPoints, rightDescriptors) = detectAndExtract(rightImage)
-
-    // Match the descriptors.
-    val matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE)
-    // A special container class for DMatch.
-    val dmatches = new MatOfDMatch
-    // The backticks are because "match" is a keyword in Scala.
-    matcher.`match`(leftDescriptors, rightDescriptors, dmatches)
-
-    // Visualize the matches and save the visualization.
-    val correspondenceImage = new Mat
-    Features2d.drawMatches(leftImage, leftKeyPoints, rightImage, rightKeyPoints, dmatches, correspondenceImage)
-    val filename = "scalaCorrespondences.png"
-    println(s"Writing ${filename}")
-    assert(Imgcodecs.imwrite(filename, correspondenceImage))
   }
 }
