9b3df42a2ca008c4201ce11b9ecd6784a35a4f59
Bill McKay <antiparagon@gmail.com>
2017-01-25 06:26:40 -0500

    Worked on DataAnalysis.R script

--- RScripts/DataAnalysis.R
+++ RScripts/DataAnalysis.R
@@ -1,1 +1,8 @@
 data <- read.csv("FastClassiferData.csv")
+library(lattice)
+with(data, xyplot(AvgY ~ AvgX, group=Symbol))
+with(data, textxy(AvgX, AvgY, Symbol))
+with(data, xyplot(AvgY ~ AvgX, group=Symbol, panel=function(x, y, ...) {
+  panel.xyplot(AvgX, AvgY, ...);
+  ltext(x=AvgX, y=AvgY, labels=Symbol, pos=1, offset=1, cex=0.8)
+}))
