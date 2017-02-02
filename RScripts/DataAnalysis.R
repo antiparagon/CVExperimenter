d742391a555c20c6572f19fdf7d998400e773f89
Bill McKay <antiparagon@gmail.com>
2017-02-02 06:15:46 -0500

    Updated DataAnalysis.R script for top feature data

--- RScripts/DataAnalysis.R
+++ RScripts/DataAnalysis.R
@@ -6,3 +6,10 @@ with(data, xyplot(AvgY ~ AvgX, group=Symbol, panel=function(x, y, ...) {
   panel.xyplot(AvgX, AvgY, ...);
   ltext(x=AvgX, y=AvgY, labels=Symbol, pos=1, offset=1, cex=0.8)
 }))
+
+
+with(data, xyplot(Feature1Y ~ Feature2X, group=Symbol))
+with(data, xyplot(Feature1Y ~ Feature1X, group=Symbol, panel=function(x, y, ...) {
+  panel.xyplot(Feature1X, Feature1Y, ...);
+  ltext(x=Feature1X, y=Feature1Y, labels=Symbol, pos=1, offset=1, cex=0.8)
+}))
