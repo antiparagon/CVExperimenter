bad903fb1eaaf3a3cccfb58a1d1a8f75121a5fc2
Bill McKay <antiparagon@gmail.com>
2017-02-08 06:06:23 -0500

    Added TrainKnnFastClassifier

---
+++ src/test/scala/com/antiparagon/cvexperimenter/chessscanner/TrainKnnFastClassifier.scala
@@ -0,0 +1,19 @@
+package com.antiparagon.cvexperimenter.chessscanner
+
+import smile.neighbor.Neighbor
+
+/**
+  * Trains a KNN classifier using the FAST training data.
+  *
+  * Created by wmckay on 2/8/17.
+  */
+object TrainKnnFastClassifier {
+
+  val TRAINING_DATA = "FastClassiferData.csv"
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+
+}
