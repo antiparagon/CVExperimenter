e10ce18a19ea3fd39f7a9c8a100dced9c5450e57
Bill McKay <antiparagon@gmail.com>
2017-02-09 06:22:31 -0500

    Working on TrainKnnFastClassifier

--- src/test/scala/com/antiparagon/cvexperimenter/chessscanner/TrainKnnFastClassifier.scala
+++ src/test/scala/com/antiparagon/cvexperimenter/chessscanner/TrainKnnFastClassifier.scala
@@ -1,5 +1,9 @@
 package com.antiparagon.cvexperimenter.chessscanner
 
+import java.io.File
+
+import smile.data.{AttributeDataset, NominalAttribute}
+import smile.data.parser.DelimitedTextParser
 import smile.neighbor.Neighbor
 
 /**
@@ -12,7 +16,10 @@ object TrainKnnFastClassifier {
   val TRAINING_DATA = "FastClassiferData.csv"
 
   def main(args: Array[String]): Unit = {
-
+    val parser = new DelimitedTextParser()
+    parser.setResponseIndex(new NominalAttribute("class"), 0)
+    val usps = parser.parse("FAST Train", new File(TRAINING_DATA))
+    //val x: Double[][] = usps.toArray(new double[usps.size()][]);
   }
 
 
