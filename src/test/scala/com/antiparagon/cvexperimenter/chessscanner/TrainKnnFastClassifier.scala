636fe62ccd6c448b9611f70af538bb547c64104f
Bill McKay <antiparagon@gmail.com>
2017-02-10 06:28:34 -0500

    Working on attribute list in TrainKnnFastClassifier

--- src/test/scala/com/antiparagon/cvexperimenter/chessscanner/TrainKnnFastClassifier.scala
+++ src/test/scala/com/antiparagon/cvexperimenter/chessscanner/TrainKnnFastClassifier.scala
@@ -2,7 +2,7 @@ package com.antiparagon.cvexperimenter.chessscanner
 
 import java.io.File
 
-import smile.data.{AttributeDataset, NominalAttribute}
+import smile.data.{AttributeDataset, NominalAttribute, NumericAttribute, StringAttribute}
 import smile.data.parser.DelimitedTextParser
 import smile.neighbor.Neighbor
 
@@ -17,9 +17,26 @@ object TrainKnnFastClassifier {
 
   def main(args: Array[String]): Unit = {
     val parser = new DelimitedTextParser()
-    parser.setResponseIndex(new NominalAttribute("class"), 0)
-    val usps = parser.parse("FAST Train", new File(TRAINING_DATA))
+    parser.setDelimiter(",")
+    parser.setColumnNames(true)
+    val aAvgX = new NumericAttribute("AvgX")
+    val aAvgY = new NumericAttribute("AvgY")
+    val aAvgResp = new NumericAttribute("AvgResp")
+    val aCoord = new StringAttribute("Coord")
+    val aSymbol = new NominalAttribute("Symbol")
+    val aImage = new StringAttribute("Image")
+
+    //val attributes = Array(new Attribute)
+    parser.setResponseIndex(aSymbol, 5)
+    val attData = parser.parse("FAST Train", new File(TRAINING_DATA))
     //val x: Double[][] = usps.toArray(new double[usps.size()][]);
+
+    println(s"Num Attr: ${attData.attributes.size}")
+
+    for(att <- attData.attributes()) {
+      println(s"Attr: ${att.getName}")
+    }
+
   }
 
 
