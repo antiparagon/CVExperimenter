c8786e87223f2ace2f0a79cb08eaefd00115941f
Bill McKay <antiparagon@gmail.com>
2017-03-02 06:20:45 -0500

    Started on ModifyFastClassifierTrainingData

---
+++ src/test/scala/com/antiparagon/cvexperimenter/chessscanner/ModifyFastClassifierTrainingData.scala
@@ -0,0 +1,22 @@
+package com.antiparagon.cvexperimenter.chessscanner
+
+import java.io.File
+import java.nio.file.{Files, Path, Paths, StandardCopyOption}
+
+
+/**
+  * Program to modify the training data created to only have the same
+  * number of instances for each piece.
+  *
+  * Created by wmckay on 3/2/17.
+  */
+object ModifyFastClassifierTrainingData {
+
+  val TRAINING_DATA = "FastClassifierData.csv"
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+
+}
